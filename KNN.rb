d858a98928e916a94bd91df603ba850f0bb8b141
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-12 18:16:55 +0100

    happy houses

---
+++ KNN.rb
@@ -0,0 +1,26 @@
+require 'matrix'
+
+def distance(v1,v2)
+  (v1-v2).magnitude
+end
+
+house_happines = {
+  Vector[56,2] => "Happy",
+  Vector[2,20] => "Not Happy",
+  Vector[18,1] => "Happy",
+  Vector[20,14] => "Not Happy",
+  Vector[30,30] => "Happy",
+  Vector[35,35] => "Happy"
+}
+
+house_1 = Vector[10,10]
+house_2 = Vector[40,40]
+
+def find_nearest(house,house_happines)
+  house_happines.sort_by{|point,v|
+    distance(house,point)
+  }.first
+end
+
+puts find_nearest(house_1,house_happines)
+puts find_nearest(house_2,house_happines)
