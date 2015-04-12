abb7deece8be7e4792802bf7dfd0e308dc19ded1
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-12 18:28:38 +0100

    KNN with k

--- KNN.rb
+++ KNN.rb
@@ -22,5 +22,17 @@ def find_nearest(house,house_happines)
   }.first
 end
 
+def find_nearest_with_k(house,house_happines,k)
+  house_happines.sort_by{|point,v|
+    distance(point,house)
+    }.first(k)
+end
+
 puts find_nearest(house_1,house_happines)
 puts find_nearest(house_2,house_happines)
+
+puts find_nearest_with_k(house_1,house_happines,3)
+puts find_nearest_with_k(house_2,house_happines,3)
+
+
+
