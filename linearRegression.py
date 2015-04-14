4fc6241fdbe45e04684f0aef4e7661810aae2b24
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-14 13:43:53 +0100

    var and cov and regression model

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt 
-from sklearn.linear_model import LiearRegression
+import numpy as np
+from sklearn.linear_model import LinearRegression
 
 x=[[10],[20],[30],[27]]
 y=[[12],[35],[55],[44]]
@@ -13,7 +14,11 @@ plt.plot(x,y,'k.')
 plt.axis([0,50,0,50])
 plt.show()
 
-model = LiearRegression()
+model = LinearRegression()
 model.fit(x,y)
 print "Predcition of price of 34 diameteer is %.2f" % model.predict([29])[0]
 
+print "Residulal sum of squares %.2f" % np.mean((model.predict(x)-y)**2)
+
+print "variance of x is %.2f" %np.var(x)
+print "cov of x and y is %.2f" %np.cov(x,y)[0][1]
