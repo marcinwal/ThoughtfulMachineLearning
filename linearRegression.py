b14515f8191d88e0be94d202f8cc712e944c18f5
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-16 14:56:02 +0100

    adding Stochastic Gradient decent

--- linearRegression.py
+++ linearRegression.py
@@ -104,8 +104,34 @@ def polynomialRegression():
   print "simple reg r-squared", regressor.score(X_test,y_test)
   print "Quadratic regression r-squared", regressor_quadratic.score(X_test_quadratic, y_test)
 
+# using Stochastic gradient decent
+def SGDDemo():
+  import numpy as np
+  from sklearn.datasets import load_boston
+  from sklearn.linear_model import SGDRegressor
+  from sklearn.cross_validation import cross_val_score
+  from sklearn.preprocessing import StandardScaler
+  from sklearn.cross_validation import train_test_split
+
+  data = load_boston()
+  X_train,X_test,y_train,y_test = train_test_split(data.data,data.target)
+
+  X_scaler = StandardScaler()
+  y_scaler = StandardScaler()
+  X_train = X_scaler.fit_transform(X_train)
+  y_train = y_scaler.fit_transform(y_train)
+  X_test = X_scaler.transform(X_test)
+  y_test = y_scaler.transform(y_test)
+
+  regressor = SGDRegressor(loss='squared_loss')
+  scores = cross_val_score(regressor,X_train,y_train,cv=5)
+  print "Cross validation r-sqr ",np.mean(scores)
+  regressor.fit_transform(X_train,y_train)
+  print "TEST score :",regressor.score(X_test,y_test)
+
 # simpleRegressionWithChart()
 # multiLinearExample()
 # multiLinearExampleWithLeastSQR()
 # regressionWithPredictions()
-polynomialRegression()
+# polynomialRegression()
+SGDDemo()
