ba69fb1b493c45bc91d046de578afbdaccf79ee8
marcin walendzik <marcin.walendzik@gmail.com>
2015-04-30 13:47:18 +0100

    multi classification

--- spam.py
+++ spam.py
@@ -158,10 +158,50 @@ def gridSearchCV():
 
 
 #not spam and ham but ,good, bad, worse etc .. more classes
+#data from http://www.kaggle.com/c/sentiment-analysis-on-movie-reviews/data
 def movieReviewsMultiClassClassification():
   import pandas as pd 
   df = pd.read_csv('./data/trainMovieSentiment.tsv',header=0,delimiter='\t')
   print df.count()
+  print df['Phrase'].head(10)
+  print df['Sentiment'].describe()
+  print df['Sentiment'].value_counts()
+  print df['Sentiment'].value_counts()/df['Sentiment'].count()
+
+  #training with sckit classifier
+  from sklearn.feature_extraction.text import TfidfVectorizer
+  from sklearn.linear_model.logistic import LogisticRegression
+  from sklearn.cross_validation import train_test_split
+  from sklearn.pipeline import Pipeline
+  from sklearn.metrics.metrics import classification_report, accuracy_score, confusion_matrix
+  from sklearn.grid_search import GridSearchCV
+
+  pipeline = Pipeline([
+    ('vect', TfidfVectorizer(stop_words='english')),
+    ('clf',LogisticRegression())
+  ])
+
+  parameters = {
+    'vect__max_df': (0.25,0.5),
+    'vect__ngram_range':((1,1),(1,2)),
+    'vect__use_idf': (True,False),
+    'clf__C':(0.01,1,10)
+  }
+
+  X,y = df['Phrase'],df['Sentiment'].as_matrix()
+  X_train,X_test,y_train,y_test = train_test_split(X,y,train_size=0.5)
+  grid_search = GridSearchCV(pipeline,parameters,n_jobs=3,verbose=1,scoring='accuracy')
+  grid_search.fit(X_train,y_train)
+  print 'Best score %0.3f' %grid_search.best_score_
+  print 'Best params set:'
+  best_parameters = grid_search.best_estimator_.get_params()
+  for param_name in sorted(parameters.keys()):
+    print '\t%s: %r' % (param_name, best_parameters[param_name])
+
+  predictions = grid_search.predict(X_test)
+  print 'accuracy:', accuracy_score(y_test,predictions)
+  print 'confusion matrix:', confusion_matrix(y_test,predictions)
+  print 'classifiaction report',classification_report(y_test,predictions)
 
 # readCollection()
 # predictions()
@@ -169,4 +209,5 @@ def movieReviewsMultiClassClassification():
 # evaluationOfClassifier() 
 # classifierPrecission()
 # gridSearchCV()
+# movieReviewsMultiClassClassification()
 movieReviewsMultiClassClassification()
