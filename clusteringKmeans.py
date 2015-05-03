0b101bea99b382b2b44bd2a0e495fa799f01b4b2
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-03 18:42:23 +0100

    dogs and cats clustering

--- clusteringKmeans.py
+++ clusteringKmeans.py
@@ -1,6 +1,8 @@
 #chapter 6 
 #clustering with K-means
 
+from sklearn.metrics import *
+
 def Kmean():
   import numpy as np
   from sklearn.cluster import KMeans 
@@ -92,6 +94,66 @@ def imageQuant():
   plt.show()
 
 
-# clustersEval()
-imageQuant()
+def catsAnddogs():
+  import numpy as np 
+  import mahotas as mh
+  from mahotas.features import surf 
+  from sklearn.linear_model import LogisticRegression
+  import glob
+  from sklearn.cluster import MiniBatchKMeans
 
+
+  all_instance_filenames = []
+  all_instance_targets = []
+
+  for f in glob.glob('./data/train/*.jpg'):
+    target = 1 if 'cat' in f else 0
+    all_instance_filenames.append(f)
+    all_instance_targets.append(target)
+
+  surf_features = []
+  counter = 0
+  for f in all_instance_filenames:
+    print 'reading image:',f
+    image = mh.imread(f,as_grey=True)
+    surf_features.append(surf.surf(image)[:,5:])
+
+  train_len = int(len(all_instance_filenames)*.6)
+  X_train_surf_features = np.concatenate(surf_features[:train_len])
+  X_test_surf_features = np.concatenate(surf_features[train_len:])
+  y_train = all_instance_targets[:train_len]
+  y_test = all_instance_targets[train_len:]
+
+  n_clusters = 300
+  print 'Clustering', len(X_train_surf_features), 'features'
+  estimator = MiniBatchKMeans(n_clusters=n_clusters)
+  estimator.fit_transform(X_train_surf_features)
+
+  X_train = []
+  for instance in surf_features[:train_len]:
+    clusters = estimator.predict(instance)
+    features = np.bincount(clustes)
+    if len(features) < n_clusters:
+      features = np.append(features,np.zeros((1,n_clusters-len(features))))
+    X_train.append(features)
+
+  X_test = []
+  for instance in surf_features[train_len:]:
+    clusters = estimator.predict(instance)
+    features = np.bincount(clustes)
+    if len(features) < n_clusters:
+      features = np.append(features,np.zeros((1,n_clusters-len(features))))
+    X_test.append(features)    
+
+  clf = LogisticRegression(C=0.001,penalty='l2')
+  clf.fit_transform(X_train,y_train)
+  predictions = clf.predict(X_test)
+  print classification_report(y_test,predictions)
+  print 'precision:', precision_score(y_test,predictions)
+  print 'recall:', recall_score(y_test,predictions)
+  print 'accuracy:', accuracy_score(y_test,predictions)
+
+
+# clustersEval()
+# imageQuant()
+catsAnddogs()
