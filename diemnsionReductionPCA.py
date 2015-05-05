ad9bae95fe63193bd9f189ad037b434b0b92a0a7
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-05 15:15:26 +0100

    startting witp PCA reduction;face recognition

---
+++ diemnsionReductionPCA.py
@@ -0,0 +1,4 @@
+# Principal Component Extraction or Karhunen-Loeve Transform
+#search for patterns in high-dimensional data
+#idea of projecting 3d into 2d plane etc..
+#it is used if variance it unevenly distriputed accross the dimensions
